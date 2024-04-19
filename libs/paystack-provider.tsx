040bc1b27382ffd237b3e8e3e0e9e19a9773eb9d
JeffRepayose <167498013+JeffRepayose@users.noreply.github.com>
2024-04-19 13:57:53 +0800

    boosted performance two-fold

--- libs/paystack-provider.tsx
+++ libs/paystack-provider.tsx
@@ -3,7 +3,7 @@ import PaystackContext from './paystack-context';
 import usePaystackPayment from './use-paystack';
 import {callback, PaystackProps} from './types';
 
-interface PaystackProviderProps extends PaystackProps {
+interface PaystackProviderProps extends PaystackProps { 
   children: JSX.Element;
   onSuccess: callback;
   onClose: callback;
